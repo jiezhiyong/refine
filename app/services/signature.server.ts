4808601f08823955b85ac0605a20e8b9d1a67bb6
jzy42459 <jzy42459@ly.com>
2024-12-27 15:59:25 +0800

    chore: update something

--- app/services/signature.server.ts
+++ app/services/signature.server.ts
@@ -1,6 +1,7 @@
 import { data } from '@remix-run/node';
+import crypto from 'crypto';
 
-// TODO: 校验签名
+// 校验签名 - webhook api
 export const validateSignature = async (request: Request) => {
   if (request.method !== 'POST') {
     return data({ message: 'Method not allowed' }, 405);
@@ -9,7 +10,7 @@ export const validateSignature = async (request: Request) => {
   const payload = await request.json();
   const signature = request.headers.get('signature');
   const generatedSignature = `sha256=${crypto
-    .createHmac('sha256', process.env.GITHUB_WEBHOOK_SECRET)
+    .createHmac('sha256', process.env.WEBHOOK_SECRET || '')
     .update(JSON.stringify(payload))
     .digest('hex')}`;
 
